b04780e4303383dc7c8fdac736621855c06eca8b
Andrey Voronov <avoronov@langate.com>
2021-02-05 18:52:19 +0200

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,18 @@ module.exports = {
     fs: "empty",
     readline: "empty",
   },
+  resolve: {
+    extensions: [".wasm", ".mjs", ".js", ".json"],
+  },
+  target: "web",
 };
+// const WorkerPlugin = require("worker-plugin");
+// const NodeTargetPlugin = require("webpack/lib/node/NodeTargetPlugin");
+
+// module.exports = (config, options) => {
+//   let workerPlugin = config.plugins.find((p) => p instanceof WorkerPlugin);
+//   if (workerPlugin) {
+//     workerPlugin.options.plugins.push(new NodeTargetPlugin());
+//   }
+//   return config;
+// };
